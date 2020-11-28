5776195cb3ec7eb7730279f0c6986b1cd903c6e6
jafetcm <jafetchaconmendez@gmail.com>
2020-11-28 01:23:58 -0600

    Now can find the closest common ancestor

--- nodes_struct/path_planning.cpp
+++ nodes_struct/path_planning.cpp
@@ -4,24 +4,9 @@
 #include <vector>
 
 using namespace std;
-/*
-struct Nodo{
-    char ID;                //Identificador del nodo
-    string description;     //Descripción del nodo 
-    char fatherID;          //ID del padre
-    int Nchild;             //Cantidad de hijos que tiene el nodo
-    char *childID;          //Arreglo de ID de hijos
-    
-};
-Nodo create_node(char _ID, string _description, char _fatherID, int _Nchild){
-    Nodo nodo;
-    nodo.ID=_ID;
-    nodo.fatherID=_fatherID;
-    nodo.description=_description;
-}
-*/
-void get_ancesterList(platform *,char);
-vector<char> path;
+void get_ancestorList(platform *,char);
+void get_path(platform *,char,char);
+vector<char> path_aux1, path_aux2, path;
 
 int main(){
     /******************* Creación de los nodos *******************/
@@ -48,20 +33,45 @@ int main(){
     /************ Impresión  de los nodos y sus padres ***********/
 
     cout<<endl;
-    //print_fatherIDlist(nodes[0],'H');
-    get_ancesterList(nodes[0],'I');
-    for (size_t i = 0; i < path.size(); i++){
-        cout << " - " << path[i];
-    }
+    get_path(nodes[0],'H','E');
+    //for (size_t i = 0; i < path.size(); i++){
+    //    cout << " -> " << path[i];
+    //}
     cout<<"\nCompila"<<endl;
     return 0;
 }
 
-void get_ancesterList(platform *nodes,char nodeID){
+void get_ancestorList(platform *nodes,char nodeID){
     nodeID = 2*(nodeID-65);
     char fatherID = nodes[nodeID].get_fatherID();
     if (fatherID != 0){
-        path.push_back(fatherID);
-        get_ancesterList(*&nodes,fatherID);
+        path_aux1.push_back(fatherID);
+        get_ancestorList(*&nodes,fatherID);
     }
+}
+
+void get_path(platform *nodes,char startID, char goalID){
+    char ancestor;
+    get_ancestorList(*&nodes,startID);              //Se obtienen todos los ancestros del nodo inicial
+    for (size_t i = 0; i < path_aux1.size(); i++){
+        path_aux2.push_back(path_aux1[i]);
+    }
+    path_aux1.clear();
+    get_ancestorList(*&nodes,goalID);               //Se obtienen todos los ancestros del nodo meta
+
+    /******************* Busqueda del ancestro común más cercano *******************/
+    //path_aux1 contiene los ancestros del nodo de destino
+    //path_aux2 contiene los ancestros del nodo de origen
+    bool contin = true;
+    for (size_t i = 0; i < path_aux2.size(); i++){
+        for (size_t j = 0; j < path_aux1.size(); j++){
+            if ((path_aux1[j] == path_aux2[i]) && contin){
+                ancestor = path_aux2[i];
+                cout << "\nAncestro común más cercano: "<<ancestor<<endl;
+                contin = false;
+            }
+        }
+    }
+    
+
 }
