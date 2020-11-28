2fcc4d5e8bf44da2e3035a225570734b9ad6c2a9
jafetcm <jafetchaconmendez@gmail.com>
2020-11-28 04:06:07 -0600

    Now can find the ful path to follow to arrive fron a start node to a goal node

--- nodes_struct/path_planning.cpp
+++ nodes_struct/path_planning.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "platform.h"
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 void get_ancestorList(platform *,char);
@@ -9,8 +10,9 @@ void get_path(platform *,char,char);
 vector<char> path_aux1, path_aux2, path;
 
 int main(){
-    /******************* Creación de los nodos *******************/
-    int num_nodos = 10;
+    char startNode,goalNode; 
+    /*#################### Creación de los nodos ####################*/
+    int num_nodos = 11;
     platform *nodes[num_nodos];
     for (size_t i = 0; i < num_nodos; i++){
         nodes[i] = new platform(i+65);
@@ -23,21 +25,26 @@ int main(){
     nodes['G'-65]->set_fatherID('C');
     nodes['H'-65]->set_fatherID('D');
     nodes['I'-65]->set_fatherID('D');
-    nodes['J'-65]->set_fatherID('G');
-    /******************* Creación de los nodos *******************/
+    nodes['J'-65]->set_fatherID('E');
+    nodes['K'-65]->set_fatherID('G');
+    /*#################### Creación de los nodos ####################*/
 
-    /************ Impresión  de los nodos y sus padres ***********/
+    /*############### Impresión  de los nodos y sus padres ##########*/
     for (size_t i = 0; i < num_nodos; i++){
         cout<<"Nodo "<<nodes[i]->get_ID()<<" - Padre: "<<nodes[i]->get_fatherID()<<endl;
     }
-    /************ Impresión  de los nodos y sus padres ***********/
+    /*############### Impresión  de los nodos y sus padres ##########*/
 
     cout<<endl;
-    get_path(nodes[0],'H','E');
-    //for (size_t i = 0; i < path.size(); i++){
-    //    cout << " -> " << path[i];
-    //}
-    cout<<"\nCompila"<<endl;
+    startNode = 'H';
+    goalNode = 'F';
+    get_path(nodes[0],startNode,goalNode);
+    cout<<"Camino para llegar de <<"<<startNode<<">> a <<"<<goalNode<<">>:"<<endl;
+    cout<<startNode;
+    for (size_t i = 0; i < path.size(); i++){
+        cout << " -> " << path[i];
+    }
+    cout<<endl;
     return 0;
 }
 
@@ -58,20 +65,34 @@ void get_path(platform *nodes,char startID, char goalID){
     }
     path_aux1.clear();
     get_ancestorList(*&nodes,goalID);               //Se obtienen todos los ancestros del nodo meta
-
-    /******************* Busqueda del ancestro común más cercano *******************/
     //path_aux1 contiene los ancestros del nodo de destino
     //path_aux2 contiene los ancestros del nodo de origen
-    bool contin = true;
+    
+    /*################### Busqueda del ancestro común más cercano ###################*/
+    path.clear();
+    bool found = false;
     for (size_t i = 0; i < path_aux2.size(); i++){
-        for (size_t j = 0; j < path_aux1.size(); j++){
-            if ((path_aux1[j] == path_aux2[i]) && contin){
+        if(!found){
+            path.push_back(path_aux2[i]);
+        }
+        for(size_t j = 0; j < path_aux1.size(); j++){
+            if ((path_aux1[j] == path_aux2[i]) && !found){
                 ancestor = path_aux2[i];
                 cout << "\nAncestro común más cercano: "<<ancestor<<endl;
-                contin = false;
+                found = true;
             }
         }
     }
-    
-
+    /*#################### Listado de ID de los nodos del camino ####################*/
+    reverse(path_aux1.begin(), path_aux1.end());    // Inversión de la lista a orden descendente
+    path_aux1.push_back(goalID);                    // Agregar el nodo meta a la lista
+    found = false;
+    for(size_t i = 0; i < path_aux1.size() ; i++){  // Iteración y seleccion de los nodos post - ancestro común
+        if(found){
+            path.push_back(path_aux1[i]);
+        }
+        if(path_aux1[i]==ancestor){
+            found = true;
+        }
+    }   
 }
