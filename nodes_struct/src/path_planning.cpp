c85b3db8ea4b426848560483a0867daf16bb17ad
jafetcm <jafetchaconmendez@gmail.com>
2020-11-28 19:08:57 -0600

    Changed dynamic objets for static, and so corrected A LOT of bugs

--- nodes_struct/src/path_planning.cpp
+++ nodes_struct/src/path_planning.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <stdlib.h>
 #include "../include/platform.h"
-//#include "platform.h"
 #include <vector>
 #include <algorithm>
 
 using namespace std;
-void get_ancestorList(platform *,char);
-void get_path(platform *,char,char);
+void get_ancestorList(platform [],char);
+void get_path(platform [],char,char);
 vector<char> path_aux1, path_aux2, path;
 int memberGlobalsize;
 
@@ -15,28 +14,28 @@ int main(){
     char startNode,goalNode; 
     /*#################### Creación de los nodos ####################*/
     int num_nodos = 11;
-    platform *nodes[num_nodos];
+    platform nodes[num_nodos];
     for (size_t i = 0; i < num_nodos; i++){
-        nodes[i] = new platform(i+65);
+        nodes[i].set_ID(i+65);
     }
-                                        nodes['A'-65]->set_childrenID("BC");
-    nodes['B'-65]->set_fatherID('A');   nodes['B'-65]->set_childrenID("DE");
-    nodes['C'-65]->set_fatherID('A');   nodes['C'-65]->set_childrenID("FG");
-    nodes['D'-65]->set_fatherID('B');   nodes['D'-65]->set_childrenID("HI");
-    nodes['E'-65]->set_fatherID('B');   nodes['E'-65]->set_childrenID("J");
-    nodes['F'-65]->set_fatherID('C');
-    nodes['G'-65]->set_fatherID('C');   nodes['G'-65]->set_childrenID("K");
-    nodes['H'-65]->set_fatherID('D');
-    nodes['I'-65]->set_fatherID('D');
-    nodes['J'-65]->set_fatherID('E');
-    nodes['K'-65]->set_fatherID('G');
+                                        nodes['A'-65].set_childrenID("BC");
+    nodes['B'-65].set_fatherID('A');    nodes['B'-65].set_childrenID("DE");
+    nodes['C'-65].set_fatherID('A');    nodes['C'-65].set_childrenID("FG");
+    nodes['D'-65].set_fatherID('B');    nodes['D'-65].set_childrenID("HI");
+    nodes['E'-65].set_fatherID('B');    nodes['E'-65].set_childrenID("J");
+    nodes['F'-65].set_fatherID('C');
+    nodes['G'-65].set_fatherID('C');    nodes['G'-65].set_childrenID("K");
+    nodes['H'-65].set_fatherID('D');
+    nodes['I'-65].set_fatherID('D');
+    nodes['J'-65].set_fatherID('E');
+    nodes['K'-65].set_fatherID('G');
 
     char *childList;
     for (size_t i = 0; i < num_nodos; i++){
-        cout<<'\t'<<char(i+65)<<" [parent: "<<nodes[i]->get_fatherID()<<"]. ("<<nodes[i]->get_childrenNum()<< " children)";
-        if(nodes[i]->get_childrenNum()>0) cout<<"\tThere are: ";
-        childList = nodes[i]->get_childrenID();
-        for (size_t j = 0; j < nodes[i]->get_childrenNum(); j++){
+        cout<<'\t'<<char(i+65)<<" [parent: "<<nodes[i].get_fatherID()<<"]. ("<<nodes[i].get_childrenNum()<< " children)";
+        if(nodes[i].get_childrenNum()>0) cout<<"\tThere are: ";
+        childList = nodes[i].get_childrenID();
+        for (size_t j = 0; j < nodes[i].get_childrenNum(); j++){
             cout<<*(childList + j)<<" ";
         }
         cout<<endl;
@@ -45,9 +44,9 @@ int main(){
     memberGlobalsize = sizeof(nodes[0]);
     cout<<"tamaño de memoria por nodo: "<<memberGlobalsize<<endl;
     
-    startNode = 'F';
+    startNode = 'H';
     goalNode = 'J';
-    get_path(nodes[0],startNode,goalNode);
+    get_path(nodes,startNode,goalNode);
     cout<<"Camino para llegar de <<"<<startNode<<">> a <<"<<goalNode<<">>:"<<endl;
     for (size_t i = 0; i < path.size(); i++){
         cout << " -> " << path[i];
@@ -56,27 +55,25 @@ int main(){
     return 0;
 }
 
-void get_ancestorList(platform *nodes,char nodeID){
-    nodeID = (sizeof(nodes[0])/memberGlobalsize)*(nodeID-65);
+void get_ancestorList(platform nodes[],char nodeID){
+    nodeID = (nodeID-65);
     char fatherID = nodes[nodeID].get_fatherID();
-    cout<<fatherID<<" - ";
-    if (fatherID != 0){
+    if (fatherID){
         path_aux1.push_back(fatherID);
-        get_ancestorList(*&nodes,fatherID);
+        get_ancestorList(nodes,fatherID);
     }
-    cout<<endl;
 }
 
-void get_path(platform *nodes,char startID, char goalID){
+void get_path(platform nodes[],char startID, char goalID){
     char ancestor;
     path_aux2.push_back(startID);
-    get_ancestorList(*&nodes,startID);              //Se obtienen todos los ancestros del nodo inicial
+    get_ancestorList(nodes,startID);              //Se obtienen todos los ancestros del nodo inicial
     for (size_t i = 0; i < path_aux1.size(); i++){
         path_aux2.push_back(path_aux1[i]);
     }
     path_aux1.clear();
     path_aux2.push_back(goalID);
-    get_ancestorList(*&nodes,goalID);               //Se obtienen todos los ancestros del nodo meta
+    get_ancestorList(nodes,goalID);               //Se obtienen todos los ancestros del nodo meta
     //path_aux1 contiene los ancestros del nodo de destino
     //path_aux2 contiene los ancestros del nodo de origen
     
